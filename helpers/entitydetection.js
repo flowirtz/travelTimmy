8650b34d44742361a5b6c264819837937ff2d42a
Marius Danner <marius.danner@student.hpi.de>
2017-10-14 11:55:14 +0200

    Retrieve location from text

--- helpers/entitydetection.js
+++ helpers/entitydetection.js
@@ -1,3 +1,20 @@
-module.exports.getCity = function(command){
-    return 'London';
+module.exports.getCity = function(text, callback){
+  var  document = {content: text, type: 'PLAIN_TEXT'};
+
+  language
+  .analyzeEntities({document: document})
+  .then(results => {
+    const entities = results[0].entities;
+    var places = []
+    entities.forEach(entity => {
+      if (entity.type == 'LOCATION') {
+        console.log(entity.name);
+        places.push(entity.name);
+      }
+    });
+    callback(places);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
 }
